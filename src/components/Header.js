3876424699795cd4f384efe107dba781ef6a5605
FazeelAhmed99 <83122423+FazeelAhmed99@users.noreply.github.com>
2021-11-15 15:33:33 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
 
             <div className="nav-bar">
                 <div className="left-nav">
-                    <img src={require('../images/newloop.png').default } alt="image not load" />
+                    <img src={require('./images/newloop.png').default } alt="image not load" />
                     <h4>Infinite Loopers</h4>
                 </div>
                 <div className="right-nav">
