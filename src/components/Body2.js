400e29c407de0e00aa0e7fd2507729d3e4cd7a77
Fazeel <fazeelahmedkhn99@gmail.com>
2021-11-15 14:29:53 +0500

    second commit

--- src/components/Body2.js
+++ src/components/Body2.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import './Body2.css';
+import loopimg from '../images/loopimg.gif'
 
 const Body2 = () => {
     return (
         <>
             <div className="body2-div">
                 <marquee>
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
-                    <img src="../images/loopimg.gif" alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
+                    <img src={loopimg} alt="image not load" id="logo-img" />
 
                 </marquee>
             </div>
